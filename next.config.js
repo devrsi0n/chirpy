a1b773f679ccdd2ade646976a7826a80476da0ae
devrsi0n <7880675+devrsi0n@users.noreply.github.com>
2021-01-02 10:32:30 +0800

    fix: yarn run analyze with multiply html opened (#51)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const { withPlugins } = require('next-compose-plugins');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+  enabled: process.env.ANALYZE === 'true' && process.env.NODE_ENV !== 'development',
 });
 
 const customConfig = {
--- src/blocks/RichTextEditor/index.tsx
+++ src/blocks/RichTextEditor/index.tsx
@@ -1,7 +1,9 @@
 import dynamic from 'next/dynamic';
 import type { IRichTextEditorProps } from './RichTextEditor';
 
-const DynamicRichTextEditor = dynamic(() => import('./RichTextEditor'));
+const DynamicRichTextEditor = dynamic(
+  () => import(/* webpackChunkName: "rich-text-editor"*/ './RichTextEditor'),
+);
 
 export function RichTextEditor(props: IRichTextEditorProps): JSX.Element {
   return <DynamicRichTextEditor {...props} />;
