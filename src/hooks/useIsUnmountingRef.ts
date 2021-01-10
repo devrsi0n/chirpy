f7babeb9448247a426e0cdae41aebea96bf97c59
Devrsi0n <7880675+devrsi0n@users.noreply.github.com>
2021-01-10 22:40:57 +0800

    fix: ssr react useLayoutEffect warning

--- src/hooks/useIsUnmountingRef.ts
+++ src/hooks/useIsUnmountingRef.ts
@@ -4,7 +4,7 @@ import * as React from 'react';
 
 export function useIsUnmountingRef(): React.MutableRefObject<boolean> {
   const isUnmountingRef = React.useRef(false);
-  React.useLayoutEffect(() => {
+  React.useEffect(() => {
     return () => {
       isUnmountingRef.current = true;
     };
