54c4b247ee097210721978b9e1e9bdd5263c7f67
devrsi0n <7880675+devrsi0n@users.noreply.github.com>
2021-01-28 22:33:03 +0800

    Login popup (#90)
    * fix: login dropdown menu style
    
    * feat: google and github login popup window

--- src/components/DropDownMenu/DropDownMenu.tsx
+++ src/components/DropDownMenu/DropDownMenu.tsx
@@ -26,9 +26,9 @@ export function DropDownMenu({
           <>
             <Menu.Button
               className={clsx(
-                'inline-flex items-center justify-center p-1 w-full text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white shadow-sm hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500 active:bg-gray-50 active:text-gray-800',
-                shape === 'circle' && 'rounded-full',
-                shape === 'square' && 'rounded-md border border-gray-300',
+                'inline-flex items-center justify-center w-full text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white shadow-sm hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500 active:bg-gray-50 active:text-gray-800',
+                shape === 'circle' && 'rounded-full p-1',
+                shape === 'square' && 'rounded-md border border-gray-300 py-3 px-2',
               )}
             >
               <span>{content}</span>
@@ -78,7 +78,7 @@ export function DropDownMenuItem(props: DropDownMenuItemProps): JSX.Element {
       {({ active }) => (
         <div
           className={clsx(
-            'flex flex-row items-center border-none text-gray-500 hover:bg-gray-100 hover:text-gray-700',
+            'flex flex-row items-center border-none text-gray-500 hover:bg-gray-100 hover:text-gray-700 cursor-pointer',
             `w-full px-6 py-2 text-sm leading-5 text-right`,
             props.className,
           )}
