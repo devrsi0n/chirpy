3f6894a140414be9999e939d5dd316b34ebde7d6
Devrsi0n <7880675+devrsi0n@users.noreply.github.com>
2021-04-29 10:54:22 +0800

    fix: access cross origin document error

--- src/hooks/useNotifyHostHeightOfPage.ts
+++ src/hooks/useNotifyHostHeightOfPage.ts
@@ -2,13 +2,18 @@ import * as React from 'react';
 
 export function useNotifyHostHeightOfPage(): void {
   React.useEffect(() => {
+    broadcastPageHeight();
     const observer = new MutationObserver(() => {
-      const newHeight: number = document.body.scrollHeight + 10;
-      window.parent.postMessage({ height: newHeight }, '*');
+      broadcastPageHeight();
     });
-    observer.observe(window.document.body, { attributes: false, childList: true, subtree: false });
+    observer.observe(window.document.body, { attributes: false, childList: true, subtree: true });
     return () => {
       observer.disconnect();
     };
   }, []);
+}
+
+function broadcastPageHeight(): void {
+  const newHeight: number = document.body.scrollHeight + 10;
+  window.parent.postMessage({ height: newHeight }, '*');
 }
