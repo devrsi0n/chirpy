925b021d9965e4f818b763e6cf55cde2373f7167
devrsi0n <7880675+devrsi0n@users.noreply.github.com>
2021-04-30 21:07:07 +0800

    fix: navigate to profile causes log out and duplicated sign in button (#176)
    * fix: navigate to profile causes log out and duplicated sign in button
    
    * fix: home page gradient text

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -25,10 +25,11 @@ export function Avatar({
   src,
   ...imgProps
 }: AvatarProps): JSX.Element {
+  const sizeStyle = sizeStyles[size];
   if (!src) {
     return (
-      <span tw="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-        <svg tw="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
+      <span css={[tw`inline-block rounded-full overflow-hidden bg-gray-200`, sizeStyle]}>
+        <svg tw="h-full w-full text-gray-400" fill="currentColor" viewBox="0 0 24 24">
           <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
         </svg>
       </span>
@@ -43,7 +44,7 @@ export function Avatar({
       className={className}
       css={[
         tw`flex items-center justify-center rounded-full bg-gray-100 select-none ring-2 ring-gray-50 dark:ring-gray-900`,
-        sizeStyles[size],
+        sizeStyle,
       ]}
     >
       {children}
