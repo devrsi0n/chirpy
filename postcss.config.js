a7a204a0e9c1ebb9f55e45a95a7f1712bd99a2dd
devrsi0n <7880675+devrsi0n@users.noreply.github.com>
2021-06-20 12:24:25 +0800

    fix: remove unused dependencies (#203)
    * fix: remove unused dependencies
    
    * fix: remove grid compatibility

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,19 @@
+/* eslint-disable unicorn/prefer-module */
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+    ],
+    'tailwindcss',
+  ],
 };
