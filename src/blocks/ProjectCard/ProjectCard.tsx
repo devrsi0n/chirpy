227826b49cb05547618acbf86dc10198c470fa56
Qing <7880675+devrsi0n@users.noreply.github.com>
2021-10-07 22:53:31 +0800

    feat(dashboard): expand pages of project card (#286)

--- src/blocks/ProjectCard/ProjectCard.tsx
+++ src/blocks/ProjectCard/ProjectCard.tsx
@@ -61,6 +61,18 @@ export function ProjectCard({ project, onDeletedProject }: ProjectCardProps): JS
       });
     }
   };
+  const [pageSize, setPageSize] = React.useState(5);
+  const pages = project.pages.slice(0, pageSize);
+  const showExpandBtn = project.pages.length > 5;
+  const isExpanded = pages.length === project.pages.length;
+
+  function handleClickExpand(): void {
+    if (pageSize === project.pages.length) {
+      return setPageSize(5);
+    }
+    const newSize = Math.min(pageSize + 5, project.pages.length);
+    setPageSize(newSize);
+  }
 
   return (
     <Card as="section" key={project.id} tw="pt-4 space-y-4">
@@ -119,26 +131,38 @@ export function ProjectCard({ project, onDeletedProject }: ProjectCardProps): JS
       </div>
       <div tw="px-6 flex flex-row space-x-2">
         <Link href={`/theme/${project.id}`} variant="plain" tabIndex={-1}>
-          <Button tw="" color="primary" shadow={false}>
+          <Button color="primary" shadow={false} tw="px-2 py-1">
             Theme
           </Button>
         </Link>
         <IntegrateGuide pid={project.id} />
       </div>
-      {project.pages.length > 0 ? (
-        <List tw="px-6">
-          {project.pages.map((page) => (
-            <Link
-              key={page.id}
-              href={page.url}
-              title={page.title || page.url}
-              variant="plain"
-              tw="inline-block w-72 overflow-ellipsis overflow-hidden whitespace-nowrap -translate-x-2"
+      {pages.length > 0 ? (
+        <div>
+          <List tw="px-4">
+            {pages.map((page) => (
+              <List.Item key={page.id} css={[listHoverable, tw``]}>
+                <Link
+                  href={page.url}
+                  title={page.title || page.url}
+                  variant="plain"
+                  tw="inline-block overflow-ellipsis overflow-hidden whitespace-nowrap"
+                >
+                  {page.title || page.url}
+                </Link>
+              </List.Item>
+            ))}
+          </List>
+          {showExpandBtn && (
+            <BaseButton
+              aria-expanded={isExpanded}
+              onClick={handleClickExpand}
+              tw="text-primary-900 hover:(bg-primary-900 text-white) rounded px-2 py-1 ml-4"
             >
-              <List.Item css={[listHoverable]}>{page.title || page.url}</List.Item>
-            </Link>
-          ))}
-        </List>
+              {!isExpanded ? 'Show more' : 'Show less'}
+            </BaseButton>
+          )}
+        </div>
       ) : (
         <Text tw="px-6" variant="secondary">
           No page integrated
