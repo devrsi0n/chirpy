7d37d44ba56b0353248bc0a5906d1f0117cbfede
Qing <7880675+devrsi0n@users.noreply.github.com>
2021-11-28 21:23:22 +0800

    fix: hasura configs

---
+++ README.md
@@ -0,0 +1,11 @@
+# Chirpy
+
+Chirpy is a easy to customize and privacy friendly comment system.
+
+## Contribution
+
+You're welcome to contribute to this project.
+
+## Development
+
+Chirpy is built upon Next.js and [Hasura](https://github.com/hasura/graphql-engine).
---
+++ hasura/seeds/default/1638093848860_table1_seed.sql
@@ -0,0 +1,7 @@
+SET check_function_bodies = false;
+INSERT INTO public."AccountProvider" (value, comment) VALUES ('Apple', '');
+INSERT INTO public."AccountProvider" (value, comment) VALUES ('Facebook', '');
+INSERT INTO public."AccountProvider" (value, comment) VALUES ('GitHub', '');
+INSERT INTO public."AccountProvider" (value, comment) VALUES ('Google', '');
+INSERT INTO public."AccountProvider" (value, comment) VALUES ('Microsoft', '');
+INSERT INTO public."AccountProvider" (value, comment) VALUES ('Twitter', '');
