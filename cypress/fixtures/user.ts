c66fce6a9a768b8638e58eac9bb23445a5435d99
Qing <7880675+devrsi0n@users.noreply.github.com>
2021-12-19 18:28:49 +0800

    fix: missing environment variables

--- cypress/fixtures/user.ts
+++ cypress/fixtures/user.ts
@@ -1,5 +1,5 @@
 export const testUser = {
-  id: +(process.env.TEST_USER_ID || 8),
+  id: process.env.TEST_USER_ID,
   name: 'CypressTest',
   email: 'cypress.test@localhost',
   image: 'https://www.cypress.io/icons/icon-72x72.png',
