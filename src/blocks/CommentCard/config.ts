f388cb2f9e39a65629ff1c90ffcd474622a9ddcc
Qing <7880675+devrsi0n@users.noreply.github.com>
2021-12-30 11:22:41 +0800

    fix(comment): deleted comment placeholder message

--- src/blocks/CommentCard/config.ts
+++ src/blocks/CommentCard/config.ts
@@ -1,8 +1,16 @@
 import { RTEValue } from '../RichTextEditor';
 
-export const PLACEHOLDER_OF_DELETED_COMMENT: RTEValue = [
-  {
-    type: 'paragraph',
-    children: [{ text: 'This comment was deleted by moderator', italic: true, underline: true }],
-  },
-];
+export const PLACEHOLDER_OF_DELETED_COMMENT: RTEValue = {
+  type: 'doc',
+  content: [
+    {
+      type: 'paragraph',
+      content: [
+        {
+          type: 'text',
+          text: '<Deleted by moderator>',
+        },
+      ],
+    },
+  ],
+};
