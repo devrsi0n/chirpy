1d23ab178706b52b0ca57d482e5f9dbf4a0c7825
Qing <7880675+devrsi0n@users.noreply.github.com>
2021-12-31 20:22:33 +0800

    fix: delete confirmation strings and router path

--- src/pages/auth/delete-confirmation.tsx
+++ src/pages/auth/delete-confirmation.tsx
@@ -6,15 +6,15 @@ import { Heading } from '$/components/Heading';
 import { APP_NAME } from '$/lib/constants';
 import { ssrMode } from '$/utilities/env';
 
-function DeletionConfirmation(): JSX.Element {
+function DeleteConfirmation(): JSX.Element {
   return (
     <>
       <Head>
-        <title>Deletion confirmation - {APP_NAME}</title>
+        <title>Delete confirmation - {APP_NAME}</title>
       </Head>
       <div tw="space-y-8">
         <Heading as="h1">
-          Thanks, you facebook account was already deleted. Status code{' '}
+          Thanks, your facebook account was deleted. Status code{' '}
           {!ssrMode ? new URLSearchParams(location.search).get('code') : ''}
         </Heading>
       </div>
@@ -22,4 +22,4 @@ function DeletionConfirmation(): JSX.Element {
   );
 }
 
-export default DeletionConfirmation;
+export default DeleteConfirmation;
