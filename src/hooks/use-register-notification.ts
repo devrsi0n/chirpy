a6414842125fab4d384e47ecaa594cd96748c660
Qing <7880675+devrsi0n@users.noreply.github.com>
2022-02-01 15:52:58 +0800

    fix: window is undefined

--- src/hooks/use-register-notification.ts
+++ src/hooks/use-register-notification.ts
@@ -5,35 +5,45 @@ import { urlBase64ToUint8Array } from '$/utilities/string';
 
 export function useRegisterNotification() {
   React.useEffect(() => {
-    if (isBrowser && 'serviceWorker' in navigator) {
+    if (!isBrowser) {
+      return;
+    }
+    if ('serviceWorker' in navigator) {
       window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/worker.js').then((registration) => {
-          registration.pushManager
-            .getSubscription()
-            .then((subscription) => {
-              if (subscription) {
-                return subscription;
-              }
-
-              const convertedVapidKey = urlBase64ToUint8Array(process.env.NEXT_PUBLIC_VAPID);
-              return registration.pushManager.subscribe({
-                // This means all push events will result in a notification
-                userVisibleOnly: true,
-                applicationServerKey: convertedVapidKey,
+        navigator.serviceWorker
+          .register('/worker.js')
+          .then((registration) => {
+            registration.pushManager
+              .getSubscription()
+              .then((subscription) => {
+                if (subscription) {
+                  return subscription;
+                }
+                const vapidKey = urlBase64ToUint8Array(process.env.NEXT_PUBLIC_VAPID);
+                return registration.pushManager.subscribe({
+                  // This means all push events will result in a notification
+                  userVisibleOnly: true,
+                  applicationServerKey: vapidKey,
+                });
+              })
+              .then((subscription) => {
+                console.log('subscription', subscription);
+                // Send the subscription details to server
+                fetch('/api/notification/register-device', {
+                  method: 'POST',
+                  headers: {
+                    'Content-type': 'application/json',
+                  },
+                  body: JSON.stringify({ subscription }),
+                });
               });
-            })
-            .then((subscription) => {
-              // Send the subscription details to server
-              fetch('/api/notification/register-device', {
-                method: 'post',
-                headers: {
-                  'Content-type': 'application/json',
-                },
-                body: JSON.stringify({ subscription: subscription }),
-              });
-            });
-        });
+          })
+          .catch((error) => {
+            console.error('Service worker registration failed', error);
+          });
       });
+    } else {
+      console.error('Service worker not supported');
     }
   }, []);
 }
