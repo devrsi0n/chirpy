09fa38221a376bf900e89d95cfd1d461f7f37886
Qing <7880675+devrsi0n@users.noreply.github.com>
2022-02-04 14:59:15 +0800

    fix: refactor the sw

--- src/external/sw/index.ts
+++ src/external/sw/index.ts
@@ -1,9 +1,9 @@
 /// <reference no-default-lib="true"/>
 /// <reference lib="esnext" />
 /// <reference lib="WebWorker" />
-import { NotificationType_Enum } from '../../server/graphql/generated/types';
 import type { NotificationPayload } from '../../server/services/notification/send';
 import { isENVDev } from '../../server/utilities/env';
+import { getTitle, openOrFocusWindow } from './utilities';
 
 const sw = self as unknown as ServiceWorkerGlobalScope & typeof globalThis;
 
@@ -14,7 +14,7 @@ sw.addEventListener('push', (event: PushEvent) => {
   const message: NotificationPayload = event.data.json();
   console.log('Push message received:', message);
   event.waitUntil(
-    sw.registration.showNotification(titleMap[message.type] + message.triggeredBy.name, {
+    sw.registration.showNotification(getTitle(message), {
       requireInteraction: isENVDev,
       icon: '/favicon.png',
       data: message,
@@ -31,36 +31,3 @@ sw.addEventListener('notificationclick', (event: NotificationEvent) => {
   const promiseChain = openOrFocusWindow(data.url);
   event.waitUntil(promiseChain);
 });
-
-/**
- * Focus on the existing tab or open a new one.
- * @param url
- */
-function openOrFocusWindow(url: string): Promise<void | WindowClient | null> {
-  const urlToOpen = new URL(url, sw.location.origin).href;
-
-  return sw.clients
-    .matchAll({
-      type: 'window',
-      includeUncontrolled: true,
-    })
-    .then((windowClients) => {
-      let matchingClient = null;
-
-      for (const windowClient of windowClients) {
-        if (windowClient.url === urlToOpen) {
-          matchingClient = windowClient;
-          break;
-        }
-      }
-
-      return matchingClient ? matchingClient.focus() : sw.clients.openWindow(urlToOpen);
-    });
-}
-
-const titleMap: Record<NotificationType_Enum, string> = {
-  ReceivedAComment: 'New comment from ',
-  ReceivedAReply: 'New reply from ',
-  ReceivedALike: 'New like from ',
-  CommentDeleted: 'Your comment was deleted by ',
-};
