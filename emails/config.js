8d8e99601cae61f3bd3cc6c3836d9e7f8aa1783f
Qing <7880675+devrsi0n@users.noreply.github.com>
2022-03-11 22:13:34 +0800

    feat: add maizzle email framework

---
+++ emails/config.js
@@ -0,0 +1,25 @@
+/*
+|-------------------------------------------------------------------------------
+| Development config               https://maizzle.com/docs/environments/#local
+|-------------------------------------------------------------------------------
+|
+| The exported object contains the default Maizzle settings for development.
+| This is used when you run `maizzle build` or `maizzle serve` and it has
+| the fastest build time, since most transformations are disabled.
+|
+*/
+
+module.exports = {
+  build: {
+    templates: {
+      source: 'src/templates',
+      destination: {
+        path: 'build_local',
+      },
+      assets: {
+        source: 'src/images',
+        destination: 'images',
+      },
+    },
+  },
+}
