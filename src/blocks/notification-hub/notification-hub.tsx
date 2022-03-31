909a2615a624e9b8260614f9d071be73f8386d47
Qing <7880675+devrsi0n@users.noreply.github.com>
2022-03-31 22:40:58 +0800

    fix: use menu instead of popover

--- src/blocks/notification-hub/notification-hub.tsx
+++ src/blocks/notification-hub/notification-hub.tsx
@@ -1,13 +1,12 @@
 import Bell from '@geist-ui/react-icons/bell';
 import * as React from 'react';
-import 'twin.macro';
 import tw from 'twin.macro';
 
 import { Avatar } from '$/components/avatar';
 import { Divider } from '$/components/divider';
 import { Heading } from '$/components/heading';
 import { Link } from '$/components/link';
-import { Popover } from '$/components/popover';
+import { Menu } from '$/components/menu';
 import { Text } from '$/components/text';
 import { useCurrentUser } from '$/contexts/current-user-context/use-current-user';
 import { useCurrentNotificationMessagesQuery } from '$/graphql/generated/notification';
@@ -23,55 +22,47 @@ export function NotificationHub(): JSX.Element {
   });
   return (
     <div tw="flex flex-row items-center mr-4">
-      <Popover
-        placement="bottomEnd"
-        styles={{
-          panel: tw`px-0`,
-        }}
-        content={
-          <div tw="space-y-5 w-max">
+      <Menu>
+        <Menu.Button>
+          <Bell size={20} />
+        </Menu.Button>
+        <Menu.Items>
+          {/* TODO: Move styles to Menu.Items once we removed the emotion */}
+          <div tw="my-4 w-max">
             <Heading as="h4" tw="font-bold px-5">
               Notifications
             </Heading>
-            {(data?.notificationMessages.length || 0) > 0 ? (
-              <ul tw="">
-                {data?.notificationMessages.map((msg, index) => (
-                  <li key={msg.id} tw="px-5 pt-3" css={[!msg.read && tw`bg-gray-300`]}>
-                    <Link
-                      href={msg.url}
-                      variant="plain"
-                      tw="flex flex-row items-start space-x-2 mb-2"
+            {data?.notificationMessages?.map((msg, index) => (
+              <Menu.Item
+                key={msg.id}
+                tw="px-5 pt-3 justify-start text-left"
+                css={[!msg.read && tw`bg-gray-300`]}
+              >
+                <Link href={msg.url} variant="plain" tw="flex flex-row items-start space-x-2 mb-2">
+                  <Avatar src={msg.triggeredBy.avatar} />
+                  <div>
+                    <Text tw="flex flex-row space-x-1.5 leading-none">
+                      <span tw="font-bold">{msg.triggeredBy.name}</span>
+                      <span>{titleMap[msg.type]}</span>
+                    </Text>
+                    <Text
+                      variant="secondary"
+                      as="time"
+                      size="xs"
+                      tw="block leading-none cursor-default mt-1"
+                      dateTime={msg.createdAt}
                     >
-                      <Avatar src={msg.triggeredBy.avatar} />
-                      <div>
-                        <Text tw="flex flex-row space-x-1.5 leading-none">
-                          <span tw="font-bold">{msg.triggeredBy.name}</span>
-                          <span>{titleMap[msg.type]}</span>
-                        </Text>
-                        <Text
-                          variant="secondary"
-                          as="time"
-                          size="xs"
-                          tw="block leading-none cursor-default mt-1"
-                          dateTime={msg.createdAt}
-                        >
-                          {dayjs(msg.createdAt).fromNow()}
-                        </Text>
-                        {msg.content && <Text tw="max-w-[15rem] mt-2">{msg.content}</Text>}
-                      </div>
-                    </Link>
-                    {index < data?.notificationMessages.length - 1 && <Divider />}
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <Text>No messages</Text>
-            )}
+                      {dayjs(msg.createdAt).fromNow()}
+                    </Text>
+                    {msg.content && <Text tw="max-w-[15rem] mt-2 italic">{msg.content}</Text>}
+                  </div>
+                </Link>
+                {index < data?.notificationMessages.length - 1 && <Divider />}
+              </Menu.Item>
+            )) || <Text>No messages</Text>}
           </div>
-        }
-      >
-        <Bell size={20} />
-      </Popover>
+        </Menu.Items>
+      </Menu>
     </div>
   );
 }
