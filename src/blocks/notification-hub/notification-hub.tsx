72d283b607067fc0f42bd09271dd1e6824ff9e9c
Qing <7880675+devrsi0n@users.noreply.github.com>
2022-04-01 21:30:46 +0800

    fix: notification message background and color

--- src/blocks/notification-hub/notification-hub.tsx
+++ src/blocks/notification-hub/notification-hub.tsx
@@ -7,11 +7,13 @@ import { Divider } from '$/components/divider';
 import { Heading } from '$/components/heading';
 import { Link } from '$/components/link';
 import { Menu } from '$/components/menu';
-import { Text } from '$/components/text';
+import { Text, TextProps } from '$/components/text';
 import { useCurrentUser } from '$/contexts/current-user-context/use-current-user';
 import { useCurrentNotificationMessagesQuery } from '$/graphql/generated/notification';
 import { NotificationType_Enum } from '$/graphql/generated/types';
 import { dayjs } from '$/utilities/date';
+
+import styles from './notification-hub.module.scss';
 
 export function NotificationHub(): JSX.Element {
   const { data: userData } = useCurrentUser();
@@ -26,45 +28,57 @@ export function NotificationHub(): JSX.Element {
         <Menu.Button>
           <Bell size={20} />
         </Menu.Button>
-        <Menu.Items>
-          {/* TODO: Move styles to Menu.Items once we removed the emotion */}
-          <div tw="my-4 w-max">
-            <Heading as="h4" tw="font-bold px-5">
-              Notifications
-            </Heading>
-            {data?.notificationMessages?.map((msg, index) => (
-              <Menu.Item
-                key={msg.id}
-                tw="px-5 pt-3 justify-start text-left"
-                css={[!msg.read && tw`bg-gray-300`]}
+        <Menu.Items className={styles.menuItems}>
+          <Heading as="h4" tw="font-bold px-5 pb-3">
+            Notifications
+          </Heading>
+          {data?.notificationMessages?.map((msg, index) => (
+            <Menu.Item
+              key={msg.id}
+              tw="px-5 pt-3 pb-0 mt-0 rounded-none hover:(rounded) flex-col items-start text-left"
+              css={[!msg.read && tw`bg-gray-200`]}
+            >
+              <Link
+                href={msg.url}
+                variant="plain"
+                tw="flex flex-row items-start space-x-2 mb-2"
+                className="group"
               >
-                <Link href={msg.url} variant="plain" tw="flex flex-row items-start space-x-2 mb-2">
-                  <Avatar src={msg.triggeredBy.avatar} />
-                  <div>
-                    <Text tw="flex flex-row space-x-1.5 leading-none">
-                      <span tw="font-bold">{msg.triggeredBy.name}</span>
-                      <span>{titleMap[msg.type]}</span>
-                    </Text>
-                    <Text
-                      variant="secondary"
-                      as="time"
-                      size="xs"
-                      tw="block leading-none cursor-default mt-1"
-                      dateTime={msg.createdAt}
-                    >
-                      {dayjs(msg.createdAt).fromNow()}
-                    </Text>
-                    {msg.content && <Text tw="max-w-[15rem] mt-2 italic">{msg.content}</Text>}
-                  </div>
-                </Link>
-                {index < data?.notificationMessages.length - 1 && <Divider />}
-              </Menu.Item>
-            )) || <Text>No messages</Text>}
-          </div>
+                <Avatar src={msg.triggeredBy.avatar} />
+                <div>
+                  <NotificationText tw="flex flex-row space-x-1.5 leading-none">
+                    <span tw="font-bold">{msg.triggeredBy.name}</span>
+                    <span>{titleMap[msg.type]}</span>
+                  </NotificationText>
+                  <NotificationText
+                    variant="secondary"
+                    as="time"
+                    size="xs"
+                    tw="block leading-none cursor-default mt-1"
+                    dateTime={msg.createdAt}
+                  >
+                    {dayjs(msg.createdAt).fromNow()}
+                  </NotificationText>
+                  {msg.content && (
+                    <NotificationText tw="max-w-[15rem] mt-2 italic">
+                      {msg.content}
+                    </NotificationText>
+                  )}
+                </div>
+              </Link>
+              {index < data?.notificationMessages.length - 1 && (
+                <Divider tw="w-full group-hover:(bg-primary-400)" />
+              )}
+            </Menu.Item>
+          )) || <Text>No messages</Text>}
         </Menu.Items>
       </Menu>
     </div>
   );
+}
+
+function NotificationText(props: TextProps): JSX.Element {
+  return <Text {...props} tw="group-hover:text-primary-1100" />;
 }
 
 const titleMap: Record<NotificationType_Enum, string> = {
