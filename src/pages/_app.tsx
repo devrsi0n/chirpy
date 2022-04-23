d60f5c482136bb2dabfa155c00695a2f19bbe433
Qing <7880675+devrsi0n@users.noreply.github.com>
2022-04-23 22:13:10 +0800

    fix: remove react-error-boundary as it blocks the rendering when there is a error in event handler

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,9 @@ import PlausibleProvider from 'next-plausible';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import * as React from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
 import 'tailwindcss/tailwind.css';
 import { GlobalStyles } from 'twin.macro';
 
-import { ErrorFallback } from '$/blocks/error-fallback';
 import { ToastProvider } from '$/components/toast';
 import { CurrentUserProvider } from '$/contexts/current-user-context';
 import { GQLClientProvider } from '$/contexts/gql-client-context';
@@ -18,38 +16,32 @@ import { ANALYTICS_DOMAIN, HASURA_TOKEN_MAX_AGE } from '$/lib/constants';
 import { appGlobalStyles } from '$/styles/global-styles';
 
 function App({ Component, pageProps: { session, ...pageProps } }: AppProps): JSX.Element {
-  const handleError = React.useCallback((error: Error, info: { componentStack: string }) => {
-    console.log({ error, info });
-  }, []);
-
   return (
-    <ErrorBoundary FallbackComponent={ErrorFallback} onError={handleError}>
-      <PlausibleProvider domain={ANALYTICS_DOMAIN}>
-        <SessionProvider
-          {...(session && { session })}
-          // Refresh hasura token before it expires
-          refetchInterval={HASURA_TOKEN_MAX_AGE - 5 * 60}
-        >
-          {/* Tailwindcss global styles */}
-          <GlobalStyles />
-          <Global styles={appGlobalStyles} />
+    <PlausibleProvider domain={ANALYTICS_DOMAIN}>
+      <SessionProvider
+        {...(session && { session })}
+        // Refresh hasura token before it expires
+        refetchInterval={HASURA_TOKEN_MAX_AGE - 5 * 60}
+      >
+        {/* Tailwindcss global styles */}
+        <GlobalStyles />
+        <Global styles={appGlobalStyles} />
 
-          <NextThemesProvider attribute="class" storageKey="chirpy.theme">
-            <LazyMotion features={loadFeatures} strict>
-              <GQLClientProvider>
-                <CurrentUserProvider>
-                  <ToastProvider>
-                    <NotificationProvider>
-                      <Component {...pageProps} />
-                    </NotificationProvider>
-                  </ToastProvider>
-                </CurrentUserProvider>
-              </GQLClientProvider>
-            </LazyMotion>
-          </NextThemesProvider>
-        </SessionProvider>
-      </PlausibleProvider>
-    </ErrorBoundary>
+        <NextThemesProvider attribute="class" storageKey="chirpy.theme">
+          <LazyMotion features={loadFeatures} strict>
+            <GQLClientProvider>
+              <CurrentUserProvider>
+                <ToastProvider>
+                  <NotificationProvider>
+                    <Component {...pageProps} />
+                  </NotificationProvider>
+                </ToastProvider>
+              </CurrentUserProvider>
+            </GQLClientProvider>
+          </LazyMotion>
+        </NextThemesProvider>
+      </SessionProvider>
+    </PlausibleProvider>
   );
 }
 
