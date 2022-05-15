af63d94cf1dc26975d2e1f18a04923833f0e5dcd
Qing Wang <wanqing@microsoft.com>
2022-05-15 19:40:45 +0800

    fix: extract a eslint config package

---
+++ packages/eslint-config-chirpy/index.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const fs = require('fs');
+const prettierConfig = JSON.parse(fs.readFileSync('./.prettierrc', 'utf8'));
+
+module.exports = {
+  plugins: ['@typescript-eslint', 'prettier', 'unicorn', 'jest'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'next/core-web-vitals',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+  ],
+  settings: {
+    next: {
+      rootDir: [path.resolve(__dirname, '../../apps/main/')],
+    },
+  },
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+
+    'prettier/prettier': ['warn', prettierConfig],
+
+    '@next/next/no-img-element': 'off',
+    '@next/next/no-html-link-for-pages': ['error', 'apps/main/src/pages/'],
+
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+
+    'unicorn/filename-case': [
+      'error',
+      {
+        case: 'kebabCase',
+        ignore: [/^\[.+\]\.tsx?$/],
+      },
+    ],
+    'unicorn/no-array-reduce': 'off',
+    'unicorn/no-null': 'off',
+    'unicorn/prevent-abbreviations': 'off',
+    'unicorn/prefer-node-protocol': 'off',
+    'unicore/prefer-module': 'off',
+    'unicorn/no-abusive-eslint-disable': 'off',
+    'unicorn/no-array-for-each': 'off',
+    'unicorn/prefer-json-parse-buffer': 'off',
+    'unicorn/import-style': 'off',
+
+    'jest/expect-expect': 'off',
+  },
+};
