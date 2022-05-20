92ce4218b80a295270976be2ef0b37f9da5ddbb7
Qing <wanqing@microsoft.com>
2022-05-20 17:45:19 +0800

    feat: integrate with axiom

--- apps/main/src/pages/_app.tsx
+++ apps/main/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { SessionProvider } from 'next-auth/react';
 import PlausibleProvider from 'next-plausible';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
+import { NextWebVitalsMetric } from 'next/app';
 import * as React from 'react';
 
 import { ToastProvider } from '$/components/toast';
@@ -39,6 +40,25 @@ function App({ Component, pageProps: { session, ...pageProps } }: AppProps): JSX
 }
 
 export default App;
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  const url = process.env.NEXT_PUBLIC_AXIOM_INGEST_ENDPOINT;
+
+  if (!url) {
+    return;
+  }
+
+  const body = JSON.stringify({
+    route: window.__NEXT_DATA__.page,
+    ...metric,
+  });
+
+  if (navigator.sendBeacon) {
+    navigator.sendBeacon(url, body);
+  } else {
+    fetch(url, { body, method: 'POST', keepalive: true });
+  }
+}
 
 export const loadFeatures = () =>
   import(
