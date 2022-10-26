1e724644f42b10c921a2270f53578511d14ff028
Qing <7880675+devrsi0n@users.noreply.github.com>
2022-10-26 21:58:50 +0800

    fix: exact shared configs

--- packages/ui/postcss.config.js
+++ packages/ui/postcss.config.js
@@ -1,15 +1,1 @@
-module.exports = {
-  plugins: {
-    'postcss-flexbugs-fixes': {},
-    'postcss-preset-env': {
-      autoprefixer: {
-        flexbox: 'no-2009',
-      },
-      stage: 3,
-      features: {
-        'custom-properties': false,
-      },
-    },
-    tailwindcss: {},
-  },
-};
+module.exports = require('configs').postcssConfig;
