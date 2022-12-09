836f4f28362ca38583439f39ca0bcf868cc547de
Qing <devrsi0n@gmail.com>
2022-12-09 19:58:31 +0800

    fix: build error

--- packages/ui/src/blocks/analytics/lazy-loader.tsx
+++ packages/ui/src/blocks/analytics/lazy-loader.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, MutableRefObject } from 'react';
 
 interface LazyLoaderProps {
   onVisible?: () => void;
@@ -7,39 +7,38 @@ interface LazyLoaderProps {
   style?: React.CSSProperties;
 }
 
-function LazyLoader(props: LazyLoaderProps): JSX.Element {
-  const element = useRef<HTMLDivElement>(null);
-  const observer = useRef<IntersectionObserver | null>(null);
+export default React.forwardRef<HTMLDivElement, LazyLoaderProps>(
+  function LazyLoader(props, ref): JSX.Element {
+    const observer = useRef<IntersectionObserver | null>(null);
 
-  useEffect(() => {
-    const elm = element.current;
-    if (!elm) {
-      return;
-    }
-    observer.current = new IntersectionObserver(
-      (entries) => {
-        if (entries[0].isIntersecting) {
-          props.onVisible && props.onVisible();
-          observer.current?.unobserve(elm);
-        }
-      },
-      {
-        threshold: 0,
-      },
+    useEffect(() => {
+      const elm = (ref as MutableRefObject<HTMLDivElement>)?.current;
+      if (!elm) {
+        return;
+      }
+      observer.current = new IntersectionObserver(
+        (entries) => {
+          if (entries[0].isIntersecting) {
+            props.onVisible && props.onVisible();
+            observer.current?.unobserve(elm);
+          }
+        },
+        {
+          threshold: 0,
+        },
+      );
+
+      observer.current.observe(elm);
+
+      return () => {
+        observer.current?.unobserve(elm);
+      };
+    }, []);
+
+    return (
+      <div ref={ref} className={props.className} style={props.style}>
+        {props.children}
+      </div>
     );
-
-    observer.current.observe(element.current);
-
-    return () => {
-      observer.current?.unobserve(elm);
-    };
-  }, []);
-
-  return (
-    <div ref={element} className={props.className} style={props.style}>
-      {props.children}
-    </div>
-  );
-}
-
-export default LazyLoader;
+  },
+);
