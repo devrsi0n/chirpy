d083bb6b02d28a3c3477062eb4250db124a45309
Qing <7880675+devrsi0n@users.noreply.github.com>
2022-12-17 03:34:38 +0000

    fix: site card link

--- packages/ui/src/blocks/site-card/site-card.tsx
+++ packages/ui/src/blocks/site-card/site-card.tsx
@@ -13,43 +13,40 @@ export function SiteCard({ site }: SiteCardProps): JSX.Element {
   const posts = site.posts.slice(0, 5);
 
   return (
-    <Card as="section" key={site.id} className="space-y-4 pt-4">
-      <Link variant="plain" className="block" href={`/site/${site.id}`}>
-        <div className="flex flex-row flex-nowrap items-center justify-between space-x-2 pl-6 pr-3">
-          <Heading as="h3">{site.name}</Heading>
+    <Card
+      as={Link}
+      href={`/site/${site.id}`}
+      variant="plain"
+      key={site.id}
+      className="block space-y-4 pt-4"
+    >
+      <div className="flex flex-row flex-nowrap items-center justify-between space-x-2 pl-6 pr-3">
+        <Heading as="h3">{site.name}</Heading>
+      </div>
+      <Text className="px-6" variant="secondary">
+        {site.subdomain}
+      </Text>
+      {posts.length > 0 ? (
+        <div>
+          <List className="px-4">
+            {posts.map((page) => (
+              <List.Item key={page.id} className={listHoverable}>
+                <Text title={page.title}>{page.title}</Text>
+              </List.Item>
+            ))}
+          </List>
         </div>
+      ) : (
         <Text className="px-6" variant="secondary">
-          {site.subdomain}
+          No posts yet
         </Text>
-        {posts.length > 0 ? (
-          <div>
-            <List className="px-4">
-              {posts.map((page) => (
-                <List.Item key={page.id} className={listHoverable}>
-                  <Link
-                    href={page.url}
-                    title={page.title || page.url}
-                    variant="plain"
-                    className="inline-block max-w-xs overflow-hidden text-ellipsis whitespace-nowrap"
-                  >
-                    {page.title || page.url}
-                  </Link>
-                </List.Item>
-              ))}
-            </List>
-          </div>
-        ) : (
-          <Text className="px-6" variant="secondary">
-            No posts yet
-          </Text>
-        )}
-        <Divider />
-        <div className="px-6 pb-4">
-          <Text className="!leading-none" size="sm" variant="secondary">
-            Created {cpDayjs(site.createdAt).fromNow()}
-          </Text>
-        </div>
-      </Link>
+      )}
+      <Divider />
+      <div className="px-6 pb-4">
+        <Text className="!leading-none" size="sm" variant="secondary">
+          Created {cpDayjs(site.createdAt).fromNow()}
+        </Text>
+      </div>
     </Card>
   );
 }
