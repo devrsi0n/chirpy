a23b9a943a7b88e8970ecc3e6bd6640103b5ccb5
Qing <7880675+devrsi0n@users.noreply.github.com>
2023-07-13 12:53:16 +0000

    fix: add missing setting-card component

---
+++ packages/ui/src/blocks/settings-card/settings-card.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+
+import { Heading } from '../../components/heading';
+
+export type SettingsCardProps = {
+  children: React.ReactNode;
+};
+
+export function SettingsCard(props: SettingsCardProps): JSX.Element {
+  return (
+    <section className="mt-10 rounded-md border">{props.children}</section>
+  );
+}
+
+SettingsCard.Header = CardHeader;
+SettingsCard.Body = CardBody;
+SettingsCard.Footer = CardFooter;
+
+export type CardTitleProps = {
+  children: React.ReactNode;
+};
+
+function CardHeader(props: CardTitleProps): JSX.Element {
+  return (
+    <Heading as="h3" className="px-5 pt-5 font-medium">
+      {props.children}
+    </Heading>
+  );
+}
+
+export type CardBodyProps = {
+  children: React.ReactNode;
+};
+
+function CardBody(props: CardBodyProps): JSX.Element {
+  return <div className="space-y-3 p-5">{props.children}</div>;
+}
+
+export type CardFooterProps = {
+  children: React.ReactNode;
+};
+
+function CardFooter(props: CardFooterProps) {
+  return (
+    <footer className="flex justify-end border-t bg-gray-300 px-5 py-3">
+      {props.children}
+    </footer>
+  );
+}
